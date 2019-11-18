c126f9a2366be1ca6eda61a045c88e5e608d7ee2
Quinton Ashley <quintonashley5@gmail.com>
2019-11-18 13:10:53 -0500

    1.4.1

--- core/updater.js
+++ core/updater.js
@@ -1,37 +1,55 @@
-const {
-	urlExists
-} = require('url-exists-promise');
+const request = require('request');
+const rp = require('request-promise-native');
 
 class Updater {
 	constructor() {}
 
+	async urlExists(url) {
+		try {
+			const res = await rp({
+				uri: url,
+				resolveWithFullResponse: true
+			});
+			return (/^(?!4)\d\d/.test(res.statusCode));
+		} catch (ror) {}
+		return false;
+	}
+
 	async check() {
-		let url = 'https://github.com/quinton-ashley/nostlan/wiki/Update-Log-v';
+		$('#dialogs').show();
+		$('#loadDialog0').text(`Checking for an update...`);
+		let url = 'https://github.com/quinton-ashley/nostlan/releases/tag/';
 		let ogVer = pkg.version.split('.');
-		let ver = ogVer;
+		for (let i in ogVer) {
+			ogVer[i] = Number(ogVer[i]);
+		}
+		let ver = [...ogVer];
 		let updateAvail = false;
-		while (await urlExists(url + `${++ver[0]}.${0}`)) {}
+		while (await this.urlExists(url + `${++ver[0]}.0.x`)) {
+			log(url + `${ver[0]}.0.x`);
+		}
 		ver[0]--;
 		if (ver[0] != ogVer[0]) {
 			ver[1] = 1;
 			updateAvail = true;
 		}
-		while (await urlExists(url + `${ver[0]}.${++ver[1]}`)) {}
+		while (await this.urlExists(url + `${ver[0]}.${++ver[1]}.x`)) {
+			log(url + `${ver[0]}.${ver[1]}.x`);
+		}
 		ver[1]--;
 		if (ver[1] != ogVer[1]) updateAvail = true;
 
 		// updateAvail = true;
 		if (!updateAvail) return;
-		let updateVer = `${ver[0]}.${ver[1]}`;
+		let updateVer = `${ver[0]}.${ver[1]}.x`;
 		url += updateVer;
-		$('#dialogs').show();
 		$('#loadDialog0').text(`Update to v${updateVer} available now!`);
 		$('#loadDialog2').text(url);
 		for (let i = 5; i > 0; i--) {
 			$('#loadDialog1').text(`Opening the link to the update log in ${i} seconds`);
 			await delay(1000);
 		}
-		opn(url);
+		opn(`https://github.com/quinton-ashley/nostlan/wiki/Update-Log-v${updateVer}`);
 		await delay(500);
 		return updateVer;
 	}
